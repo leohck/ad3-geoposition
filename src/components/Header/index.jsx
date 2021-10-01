8f6e3b0f4538e638933e14428d054edbd33bbc62
leonardo black <leonardoblack.net@gmail.com>
2021-09-30 23:49:47 -0300

    update project to deploy

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 export const Header = () => (
     <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">Ad3Plus</a>
+            <h2>Ad3Plus</h2>
         </div>
     </nav>
 );
